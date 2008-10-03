03ea6bef1c7371255aa605481fc7eed1af412546
nevans <admin@thought-workers-macbook-pro-3.local>
2008-10-03 13:29:16 -0700

    integrated the check task with taglob

--- lib/taglob/rake/check_tags_task.rb
+++ lib/taglob/rake/check_tags_task.rb
@@ -1,11 +1,12 @@
 require 'rake'
 require 'rake/tasklib'
+require 'taglob'
 
 module Taglob
   module Rake
     class CheckTagsTask < ::Rake::TaskLib
-      
       attr_accessor :pattern
+      attr_accessor :valid_tag_source
       def initialize(name = :check_tags)
         @name = name
         yield self if block_given?
@@ -14,8 +15,15 @@ module Taglob
       
       def define
         task @name do
+          invalid_tags = Taglob.invalid_tags(pattern,valid_tag_list)
+          exit 1 if !invalid_tags.empty?
         end
       end
+      
+      def valid_tag_list
+        var = File.readlines(valid_tag_source).collect {|line| line.chomp }
+      end
+      
     end
   end
 end
--- spec/check_tags_task_spec.rb
+++ spec/check_tags_task_spec.rb
@@ -3,13 +3,13 @@ require 'taglob/rake'
 
 describe Taglob::Rake::CheckTagsTask do
   
-  before :all do
+  before :each do
     @file_name ="./lib/taglob/rake/check_tags_task.rb"
     @rake = Rake::Application.new
     Rake.application = @rake
   end
   
-  after :all do
+  after :each do
     Rake.application = nil 
   end
   
@@ -26,6 +26,26 @@ describe Taglob::Rake::CheckTagsTask do
     task.pattern.should eql(pattern)
   end
   
-  it "should raise error if invalid tags present" 
-  it "should not raise error if only valid tags present"
+  it "should build valid tag list from provided file" do
+    task = Taglob::Rake::CheckTagsTask.new do |t|
+      t.valid_tag_source = 'spec/invalid_tags.txt'
+    end
+    task.valid_tag_list.should eql(['foo'])
+  end
+  
+  it "should raise error if invalid tags present" do
+    task = Taglob::Rake::CheckTagsTask.new :error_out do |t|
+      t.pattern = 'spec/tagged_files/*.rb'
+      t.valid_tag_source = 'spec/invalid_tags.txt'
+    end
+    lambda {@rake.invoke_task('error_out')}.should raise_error(SystemExit)
+  end
+  
+  it "should not raise error if only valid tags present" do
+    task = Taglob::Rake::CheckTagsTask.new :no_errors do |t|
+      t.pattern = 'spec/tagged_files/*.rb'
+      t.valid_tag_source = 'spec/valid_tags.txt'
+    end
+    lambda {@rake.invoke_task('no_errors')}.should_not raise_error(SystemExit)
+  end
 end
